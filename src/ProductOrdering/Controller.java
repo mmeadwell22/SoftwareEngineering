b62488a1a4053923ab7dfb7c51a8a61c63d600c1
Matthew Meadwell <mmeadwell22@gmail.com>
2020-02-26 12:54:10 -0500

    simple code demonstrating how to make connection to local SQL databse
    simple code to run a query on a local SQL database

--- src/ProductOrdering/Controller.java
+++ src/ProductOrdering/Controller.java
@@ -1,4 +1,37 @@
 package ProductOrdering;
 
+import javafx.fxml.FXML;
+import java.sql.*;
+
+import java.sql.SQLException;
+
 public class Controller {
+
+    Connection connection = null;
+    Statement stm = null;
+    ResultSet result = null;
+    @FXML
+    protected void onClick(){
+        try{
+            Connection connection = null;
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String connectionUrl = "jdbc:sqlserver://localhost:1433;" + "databaseName=OrderSystem;user=program;password=;";
+            connection = DriverManager.getConnection(connectionUrl);
+            System.out.println("Connected to database");
+
+            String SQL = "SELECT * FROM dbo.Catalog";
+            Statement stm = connection.createStatement();
+            ResultSet result = stm.executeQuery(SQL);
+
+            while(result.next()){
+                System.out.println(result.getString(1) + "  " + result.getString(2) + "  " + result.getString(3));
+            }
+
+        }catch(ClassNotFoundException e){
+            System.out.println("Class not found exception " + e.getMessage());
+        }
+        catch(SQLException e){
+            System.out.println("SQL exception " + e.getMessage());
+        }
+    }
 }
--- src/ProductOrdering/Main.java
+++ src/ProductOrdering/Main.java
@@ -1,23 +1,54 @@
 package ProductOrdering;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import java.sql.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import javax.swing.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1920, 1080));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("testing actions");
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
 
+/*    class OKHandler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent e){
+            System.out.println("Ok button clicked");
+        }
+    }
+
+    class CancelHandler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent e){
+            System.out.println("Cancel button clicked");
+        }
+    }*/
+
+
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
