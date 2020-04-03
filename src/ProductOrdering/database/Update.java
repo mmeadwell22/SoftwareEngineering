08231810cda9e010adb2f42386724a885a031a5f
Noel Mulcahy <n.mulcahy@vikes.csuohio.edu>
2020-04-03 12:57:57 -0400

    Changes to the Select Class
    Now, table items are selected from their specific function
    
    i.e Select.uniqueOrder(id: String) or Select.allCustomers()

--- src/ProductOrdering/database/Update.java
+++ src/ProductOrdering/database/Update.java
@@ -4,156 +4,129 @@ import java.sql.*;
 
 public abstract class Update extends DatabaseUtil {
 
-    public void updateCustomer( String CID, String name, String contact, String address, String payment, String company) throws SQLException, ClassNotFoundException {
-        updateCustomerName(name, CID);
-        updateCustomerContact(contact, CID);
-        updateCustomerAddress(address, CID);
-        updateCustomerPayment(payment, CID);
-        updateCustomerCompany(company, CID);
+    public void customer(String CID, String name,
+                               String contact, String address,
+                               String payment, String company)
+            throws SQLException, ClassNotFoundException {
+        customerName(name, CID);
+        customerContact(contact, CID);
+        customerAddress(address, CID);
+        customerPayment(payment, CID);
+        customerCompany(company, CID);
     }
-    public void  updateCustomerName(String name, String CID) {
-        if (connection == null) {
-        try {
-           connect();
 
-            String SQL = "Update dbo.Customer Set CName =" + name +"where CID =" +CID;
-            Statement stm = connection.createStatement();
-            ResultSet result = stm.executeQuery(SQL);
-            
-            disconnect();
+    public void customerName(String name, String CID) {
+        if (connection == null) {
+            try {
+                connect();
+
+                String SQL = "Update dbo.Customer Set CName =" + name + "where CID =" + CID;
+                Statement stm = connection.createStatement();
+                ResultSet result = stm.executeQuery(SQL);
+            } catch (SQLException e) {
+                System.out.println("SQL exception " + e.getMessage());
             }
-        catch(SQLException e){
-            System.out.println("SQL exception " + e.getMessage());
-            }
-            
-        }
-        else {      //disconnect then, recursively call
             disconnect();
-            updateCustomerName(name, CID);
         }
     }
 
-    public void updateCustomerContact (String contact, String CID) {
+    public void customerContact(String contact, String CID) {
         if (connection == null) {
+            try {
+                connect();
+
+                String SQL = "Update dbo.Customer Set contact = " + contact + "where CID =" + CID;
+                Statement stm = connection.createStatement();
+                ResultSet result = stm.executeQuery(SQL);
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                System.out.println("SQL exception " + e.getMessage());
+            }
+            disconnect();
+        }
+    }
+
+    public void customerAddress(String address, String CID) {
         try {
             connect();
-
-            String SQL = "Update dbo.Customer Set contact =" +contact + "where CID =" +CID;
-            Statement stm = connection.createStatement();
-            ResultSet result = stm.executeQuery(SQL);
-            disconnect();
-            }
-        catch(SQLException e){
-            System.out.println("SQL exception " + e.getMessage());
-            }
-        }
-         else {      //disconnect then, recursively call
-            disconnect();
-            updateCustomerContact(contact, CID);   
-         }
-    }
-
-   public void updateCustomerAddress (String address, String CID) {
-        try {
-           connect()
 
             String SQL = "Update dbo.Customer Set [Address] =" + address + "where CID =" + CID;
             Statement stm = connection.createStatement();
             ResultSet result = stm.executeQuery(SQL);
             disconnect();
-            }
-        catch(SQLException) {
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
             System.out.println("SQL exception " + e.getMessage());
-             }
-        else {      //disconnect then, recursively call
-            disconnect();
-            updateCustomerAddress(address, CID);
         }
     }
 
-    public void updateCustomerPayment (String payInfo, String CID) {
+    public void customerPayment(String payInfo, String CID) {
         if (connection == null) {
-        try {
-            connect();
+            try {
+                connect();
 
-            String SQL = "Update dbo.Customer Set Payment_info =" +payInfo + "where CID =" + CID;
-            Statement stm = connection.createStatement();
-            ResultSet result = stm.executeQuery(SQL);
-            disconnect();
-            }
-        catch(SQLException) {
-            System.out.println("SQL exception " + e.getMessage());
+                String SQL = "Update dbo.Customer Set Payment_info =" + payInfo + "where CID =" + CID;
+                Statement stm = connection.createStatement();
+                ResultSet result = stm.executeQuery(SQL);
+                disconnect();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                System.out.println("SQL exception " + e.getMessage());
             }
         }
-         else {      //disconnect then, recursively call
-            disconnect();
-            updateCustomerPaymenet(payInfo, CID);   
-         }
     }
 
-    public void updateCustomerCompany (String company, String CID) {
-        if (connection = null) {
-        try {
-           connect();
+    public void customerCompany(String company, String CID) {
+        if (connection == null) {
+            try {
+                connect();
 
-            String SQL = "Update dbo.Customer Set company =" + company+ "where CID =" +CID;
-            Statement stm = connection.createStatement();
-            ResultSet result = stm.executeQuery(SQL);
-            
-            disconnect();
+                String SQL = "Update dbo.Customer Set company =" + company + "where CID =" + CID;
+                Statement stm = connection.createStatement();
+                ResultSet result = stm.executeQuery(SQL);
+
+                disconnect();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                System.out.println("SQL exception " + e.getMessage());
             }
-        catch(SQLException) {
-            System.out.println("SQL exception " + e.getMessage());
-            }           
         }
-         else {      //disconnect then, recursively call
-            disconnect();
-            updateCustomerCompany(company, CID);
-         }
     }
 
     //method for updating the Order_info table
-    public void updateOrder (String newAddress, String Order_ID) {
-        if (connection = null) {
-        try {
-            connect();
+    public void order(String newAddress, String Order_ID) {
+        if (connection == null) {
+            try {
+                connect();
 
-            String SQL = "Update dbo.Order_info Set Delivery_address =" + newAddress  +
+                String SQL = "Update dbo.Order_info Set Delivery_address =" + newAddress +
                         "where Order_ID =" + Order_ID;
-            Statement stm = connection.createStatement();
-            ResultSet result = stm.executeQuery(SQL);
-            
-            disconnect();
+                Statement stm = connection.createStatement();
+                ResultSet result = stm.executeQuery(SQL);
+
+                disconnect();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                System.out.println("SQL exception " + e.getMessage());
             }
-        catch(SQLException) {
-            System.out.println("SQL exception " + e.getMessage());
-            }    
         }
-         else {      //disconnect then, recursively call
-            disconnect();
-            updateOrder(newAddress, Order_ID);   
-         }
     }
-    
-    public void updateProductName (String Pname, String PID) {
-        if (connection = null) {
-        try {
-            connect();            
-            String SQL = "Update dbo.Product Set Product_Name =" + Pname +
+
+    public void productName(String Pname, String PID) {
+        if (connection == null) {
+            try {
+                connect();
+
+                String SQL = "Update dbo.Product Set Product_Name =" + Pname +
                         "where PID =" + PID;
-            Statement stm = connection.createStatement();
-            ResultSet result = stm.executeQuery(SQL);
-            
-            disconnect();
+                Statement stm = connection.createStatement();
+                ResultSet result = stm.executeQuery(SQL);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                System.out.println("SQL exception " + e.getMessage());
             }
-        catch(SQLException) {
-            System.out.println("SQL exception " + e.getMessage());
-            }       
-        }
-        else {
-            disconnect();
-            updateProductName(Pname, PID);
         }
     }
-    
+    //end of Quang's code
 }
